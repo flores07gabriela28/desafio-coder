e6ce95afaf8c95e70fbe392dabf394bb2802905a
flores07gabriela28 <97180542+flores07gabriela28@users.noreply.github.com>
2022-08-31 22:36:14 -0300

    first commit

---
+++ clase.js
@@ -0,0 +1,31 @@
+let numero= Number(prompt(`Cual es su edad`))
+if (numero >=18){
+alert("Usted puede ingresar")
+}
+else if(numero <18){
+    alert("Es muy joven para ingresar")
+}
+else{
+    alert("Eso no es un número")
+}
+
+let i=0
+while (i < 10) {
+    console.log(i)
+    i++;
+}
+
+
+
+
+
+
+//let cant= parseInt( prompt("Ingrese cantidad de goles anotados"))
+//let cantPartidos= parseInt( prompt("Ingrese cantidad de partidos jugados"))
+//let promedio= (cant/cantPartidos)
+
+//console.log( `El promedio del jugador ${nombreJugador} es ${promedio}`)
+//alert(`Del jugador ${nombreJugador} el promedio es ${promedio}`)
+
+
+
