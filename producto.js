2f4adaf3f5d93a034c078b8ed099b0a92b7a2c1a
flores07gabriela28 <97180542+flores07gabriela28@users.noreply.github.com>
2022-11-26 23:42:44 -0300

    3 entrega

--- producto.js
+++
@@ -1,9 +0,0 @@
-class Producto{
-    constructor(id,nombre,precio)
-    {
-        this.id=id;
-        this.nombre=nombre;
-        this.precio=precio;
-    }
-}
-
---
+++ shopping/src/stock.js
@@ -0,0 +1,70 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Maintenance Criadores Perro Adulto",
+        tipo: "Alimento seco adulto",
+        desc: "Alimento pensado para perros adultos de todas las razas y tamaños a partir de 12 meses de edad con actividad física moderada a alta. Sus principales características son: alta palatabilidad, protección de la salud del pelo y la piel, fortalecimiento de huesos y articulaciones e incremento de las defensas.",
+        precio: 4500,
+        kg: "22",
+        img: './public/images/alimentoMaintenance.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Dog Chow S/ Colorante",
+        tipo: "Alimento seco adulto",
+        desc: "Dog Chow para perros adultos de razas medianas y grandes es un alimento balanceado que maximiza la calidad de vida de tu mascota. Equilibra la flora intestinal, promueve un pelaje brillante y contribuye a un buen desarrollo del cerebro y la visión. Nutrición especificamente diseñada para tu perro adulto mediano y grande de 1 a 7 años de edad, que ayuda a mejorar su calidad de vida de forma visible, con una receta sin colorantes.",
+        precio: 8000,
+        kg: "21",
+        img: './public/images/alimentoDogChow.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Pedigree Adulto Carne Pollo Y Cereales",
+        tipo: "Alimento seco adulto",
+        desc: "Pedigree carne, pollo y cereales es un alimento balanceado para perros adultos de 1 a 7 años que contiene los nutrientes que necesita para un correcto desarrollo en su madurez, ya que aporta un equilibrio óptimo de fibras, minerales y proteínas. Pedigree Adulto aporta una nutrición profesional para cada etapa de su vida, cubriendo las 4 necesidades universales. Contiene un balance óptimo de fibra y proteínas de alta calidad que contribuyen a un sistema digestivo saludable y heces más firmes. Cuenta con antioxidantes, como la vitamina E, que contribuye a mantener sus defensas saludables. Ayuda a la limpieza de sus dientes, a reducir la formación del sarro y a que sus encías estén más saludables. Contiene minerales como el Zinc y aceites esenciales como el Omega 6, para nutrir su piel y que su pelo esté más brilloso.",
+        precio: 6000,
+        kg: "21",
+        img: './public/images/alimentoPedigree.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Dog Selection Criadores Carne Y Pollo",
+        tipo: "Alimento seco adulto",
+        desc: "Dog Selection Criadores por 21 + 3 kg es un alimento seco y balanceado sabor carne y pollo para perros adultos mayores a los 12 meses de edad. Al contener proteínas de alta calidad, minerales y ácidos grasos ricos en Omega 3 y 6, favorece a mantener la piel sana y a estimular el crecimiento del pelo fuerte y brillantes.",
+        precio: 5800,
+        kg: "24",
+        img: './public/images/alimentoDogSelection.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Origen By Company",
+        tipo: "Alimento seco adulto",
+        desc: "Origen By Company para perros adultos es un alimento balanceado que proporciona los nutrientes necesarios para un crecimiento fuerte y sano de tu mascota. Su aporte de nutrientes, proteínas, zinc y hierro son esenciales para maximizar y mejorar su calidad.",
+        precio: 3800,
+        kg: "23",
+        img: './public/images/alimentoOrigen.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Agility Derma Control",
+        tipo: "Alimento seco adulto",
+        desc: "Agility Derma Control es un alimento completo y balanceado para perros adultos a partir de los 12 meses de vida en adelante. Está especialmente formulado con proteínas de cerdo, que ayuda a disminuir las probabilidades de aparición de alteraciones en la piel asociadas a la intolerancia digestiva. Los ácidos grasos Omega 3 y 6 ayudan a reforzar el estado óptimo de la piel y otorga brillo a al pelaje de tu fiel compañero. Las proteínas de alta digestibilidad y el bajo contenido en grasas saturadas contribuyen al mantenimiento de la masa muscular magra. Los prebióticos promueven el desarrollo de una flora bacteriana saludable favoreciendo la absorción de nutrientes. Entre sus principales beneficios se destacan: Al contener cerdo reduce la posibilidad de alteraciones de la piel por intolerencia alimentaria.",
+        precio: 5700,
+        kg: "15",
+        img: './public/images/alimentoAgility.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
